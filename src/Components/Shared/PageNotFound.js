f167a6a177da0f94a81241543deb1cf78293bab1
artebleker <artebleker@gmail.com>
2022-05-09 16:49:01 -0300

    final commit

--- src/Components/Shared/PageNotFound.js
+++ src/Components/Shared/PageNotFound.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import PageNotFoundImage from '../../assets/contentImages/404.jpg'
 import './PageNotFound.css'
-
+import LayoutPublic from '../LayoutPublic/LayoutPublic'
 const PageNotFound = () => {
   return (
+    <LayoutPublic>
     <div className='container-404'>
         <h1 className='title-404'>La ruta ingresada no se encuentra registrada</h1>
         <img src={PageNotFoundImage} alt="" className='image-404'/>
         <p className='subtitle-404'>Revisa que el path ingresado sea el indicado y recarga nuevamente la página web</p>
     </div>
+    </LayoutPublic>
   )
 }
 
